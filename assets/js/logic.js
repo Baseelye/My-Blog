4dd2dcfd6d3207d073d4a094e0efe846afca3fe6
Baseelye <165911977+Baseelye@users.noreply.github.com>
2024-05-11 05:55:04 -0400

    value get saved into local storage

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,12 +1,32 @@
 let formEl = document.getElementById("submitting")
-console.log(formEl)
+console.log(formEl);
+let listBlog = [];
 
+let insideLocal = JSON.parse(localStorage.getItem("Blogs"))
+console.log(insideLocal)
+if(insideLocal){
+    listBlog = insideLocal
+}
 
 
 formEl.addEventListener("submit", function(event){
 event.preventDefault();
-let username = document.getElementById("username").value
-console.log(username)
+let username = document.getElementById("username").value;
+let title = document.getElementById("title").value;
+let content = document.getElementById("content").value;
+if (!username || !title || !content) {
+    window.alert ('You need to complete the form');
+    return;
+}
+let inputData = {
+    username, title, content
+}
+listBlog.push(inputData)
+console.log(listBlog);
 
+localStorage.setItem("Blogs", JSON.stringify(listBlog));
 
-})
+location.href = "./blog.html"
+
+});
+
